600acdbb13cf0497d1820190bb4ba371fbdc46ea
Felipe Chagas Bonadio <felipe.bonadio@etec.sp.gov.br>
2021-10-15 15:29:31 -0300

    Adicionado Teste3

--- src/test/java/uri1002/CircleTest.java
+++ src/test/java/uri1002/CircleTest.java
@@ -9,7 +9,7 @@ public class CircleTest {
 	@Test
 	public void testGetArea() {
 		Circle c = new Circle();
-		c.setRadius(2);
+		c.setRadius(2.00);
 		double expected = 12.5664;
 		double actual = c.getArea();
 		assertEquals(expected, actual, 0.0001);
@@ -19,6 +19,14 @@ public class CircleTest {
 		Circle c = new Circle();
 		c.setRadius(100.64);
 		double expected = 31819.3103;
+		double actual = c.getArea();
+		assertEquals(expected, actual, 0.0001);
+	}
+	
+	public void testGetArea3() {
+		Circle c = new Circle();
+		c.setRadius(150.00);
+		double expected = 70685.7750;
 		double actual = c.getArea();
 		assertEquals(expected, actual, 0.0001);
 	}

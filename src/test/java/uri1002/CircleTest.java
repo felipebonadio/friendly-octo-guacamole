ea2f994434b777a5e90391fa04ecd2980eb3c4ff
Felipe Chagas Bonadio <felipe.bonadio@etec.sp.gov.br>
2021-10-15 15:26:42 -0300

    Alteracao Teste

--- src/test/java/uri1002/CircleTest.java
+++ src/test/java/uri1002/CircleTest.java
@@ -14,5 +14,13 @@ public class CircleTest {
 		double actual = c.getArea();
 		assertEquals(expected, actual, 0.0001);
 	}
+	
+	public void testGetArea2() {
+		Circle c = new Circle();
+		c.setRadius(100.64);
+		double expected = 31819.3103;
+		double actual = c.getArea();
+		assertEquals(expected, actual, 0.0001);
+	}
 
 }
